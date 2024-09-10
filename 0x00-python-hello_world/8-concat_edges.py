0ccabb90bdb54808c443d1098e86d73ea9b72266
martinondiwa <surekondiwa@gmail.com>
2024-09-10 14:41:40 -0700

    8 concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-# Initialize the string with a description of Python
 str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-
-# Extract and concatenate substrings
-# First extract: "object-oriented programming" (index 39 to 67)
-# Second extract: "with " (index 107 to 112)
-# Third extract: "Python" (first 6 characters)
-str = str[39:67] + str[107:112] + str[:6]
-
-# Print the resulting concatenated string
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
