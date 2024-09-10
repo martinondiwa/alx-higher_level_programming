900d62073f95f17a45a681f718df48aa80ccb315
martinondiwa <surekondiwa@gmail.com>
2024-09-10 15:06:10 -0700

    float

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-number = 3.14159
-print('Float: {:.2f}'.format(number))
+
+number = 3.14159  # You can change this number to test with different values
+
+# Print the float with a precision of 2 digits
+print(f"Float: {number:.2f}")
