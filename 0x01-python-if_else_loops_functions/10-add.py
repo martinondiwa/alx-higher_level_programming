2846ed5bef1777afc3af700b238cbf6e3afd88e2
martinondiwa <surekondiwa@gmail.com>
2024-09-10 12:52:42 -0700

    add 2 nums

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Adds two numbers and returns the result.
+
+    Args:
+        a: First number.
+        b: Second number.
+
+    Returns:
+        The sum of a and b.
+    """
+    return a + b
