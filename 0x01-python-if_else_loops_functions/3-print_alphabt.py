5bb6a702abc542e1da08ea6680e0a5ca8cd1065a
martinondiwa <surekondiwa@gmail.com>
2024-09-10 13:51:30 -0700

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
-# Loop through ASCII values of lowercase letters (a to z)
-for i in range(97, 123):
-    # Skip the letters 'e' (ASCII 101) and 'q' (ASCII 113)
-    if (i == 101) or (i == 113):
-        continue
-    # Print the character corresponding to the ASCII value
-    print(chr(i), end="")
+for x in range(ord('a'), ord('z') + 1):
+    if x != ord('q') and x != ord('e'):
+        print("{:c}".format(x), end='')
