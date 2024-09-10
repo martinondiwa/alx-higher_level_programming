274edbd58722f175223ac6c3810b8a25b67b8028
martinondiwa <surekondiwa@gmail.com>
2024-09-10 12:11:41 -0700

    Printing alphabet

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Loop through ASCII values of lowercase letters (a to z)
+for i in range(97, 123):
+    # Print the character corresponding to the ASCII value
+    
+    print(chr(i), end="")
