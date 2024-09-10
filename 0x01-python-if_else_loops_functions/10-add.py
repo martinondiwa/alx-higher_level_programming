bb5f6bef26f6b1243029a3e54c9b0fde02ac8003
martinondiwa <surekondiwa@gmail.com>
2024-09-10 14:21:24 -0700

    add

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,12 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def add(a, b):
-    """
-    Adds two numbers and returns the result.
-
-    Args:
-        a: First number.
-        b: Second number.
-
-    Returns:
-        The sum of a and b.
-    """
-    return a + b
+    """Return the addition of a and b."""
+    return (a + b)
