8e3ae6bf1700f2e2797ac9c7e3ebea767574f779
martinondiwa <surekondiwa@gmail.com>
2024-09-10 14:19:10 -0700

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+# 9-print_last_digit.py
+# Brennan D Baraban <375@holbertonschool.com>
 
-# Import the function 'print_last_digit' from '9-print_last_digit' module
-print_last_digit = __import__('9-print_last_digit').print_last_digit
 
-# Call the function with 98 and 0 as arguments
-print_last_digit(98)
-print_last_digit(0)
-
-# Call the function with -1024 and store the result in 'r'
-r = print_last_digit(-1024)
-
-# Print the value of 'r' (the last digit of -1024)
-print(r)
+def print_last_digit(number):
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
