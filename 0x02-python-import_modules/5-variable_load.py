e7fc139cf7ef8613d0832087145ae6e451392832
martinondiwa <surekondiwa@gmail.com>
2024-09-12 13:14:45 -0700

    variable load

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# This script prints the value of variable 'a' from the module variable_load_5.
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    # Print the value of 'a'
+    print(a)
