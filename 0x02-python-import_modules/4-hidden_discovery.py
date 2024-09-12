989f15e0d3d6f30b933ce8e845322fe13e87dd16
martinondiwa <surekondiwa@gmail.com>
2024-09-12 13:45:51 -0700

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-# This script prints all names defined by the hidden_4 module, excluding those starting with '__'.
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-    # Get a list of all names defined by hidden_4
-    names = dir(hidden_4)
-
-    # Loop through the names and print those that don't start with '__'
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    discovr()
