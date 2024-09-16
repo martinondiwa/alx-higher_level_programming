8f0126ce4823ed1b2c823c85f114228545986880
martinondiwa <surekondiwa@gmail.com>
2024-09-16 12:55:01 -0700

    cdfe

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,33 +1,17 @@
-#include <Python.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
 
 /**
- * print_python_list_info - print info about a Python list object
- * @p: pointer to PyObject, which should be castable to a PyListObject
+ * print_python_list_info - prints info about python lists
+ * @p: address of pyobject struct
  */
 void print_python_list_info(PyObject *p)
 {
-    if (!PyList_Check(p))
-    {
-        fprintf(stderr, "Provided PyObject is not a list\n");
-        return;
-    }
+	int i;
 
-    PyListObject *list_obj = (PyListObject *)p;
-    Py_ssize_t size = PyList_Size(p);
-    Py_ssize_t allocated = PyList_GET_SIZE(p);  // This gives us the allocated size, not always visible
-
-    // Print the size of the list
-    printf("[*] Size of the Python List = %zd\n", size);
-
-    // Print allocated size
-    printf("[*] Allocated = %zd\n", allocated);
-
-    // Iterate through the list and print the type of each element
-    for (Py_ssize_t i = 0; i < size; i++)
-    {
-        PyObject *item = PyList_GetItem(p, i);
-        const char *type = Py_TYPE(item)->tp_name;
-        printf("Element %zd: %s\n", i, type);
-    }
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
