8dea6935e0dbb02cf96fdccaef56c7e6a2368ec1
martinondiwa <surekondiwa@gmail.com>
2024-09-16 09:58:41 -0700

    com

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
-
 def new_in_list(my_list, idx, element):
-    """
-    Creates a new list with an element replaced at a specified index.
-
-    Args:
-        my_list (list): The original list to modify.
-        idx (int): The index at which to replace the element.
-        element (any): The new element to insert into the list.
-
-    Returns:
-        list: A new list with the element replaced if the index is valid;
-              otherwise, a copy of the original list.
-    """
     if idx < 0 or idx >= len(my_list):
-        return list(my_list)
-    
-    # Create a copy of the original list
-    new_list = list(my_list)
-    new_list[idx] = element
-    
-    return new_list
+        return my_list
+
+    my_list_copy = my_list.copy()
+    my_list_copy[idx] = element
+    return my_list_copy
