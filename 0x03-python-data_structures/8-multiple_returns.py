eccff8747b803c6dbb4b733e949e3c8bd47aa1ab
martinondiwa <surekondiwa@gmail.com>
2024-09-16 10:06:42 -0700

    asfghj

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-    if sentence:
-        sen_len = len(sentence)
-    else:
-        sen_len = 0
-    return (sen_len, sentence if not sentence else sentence[:1])
-
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
