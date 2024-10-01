66edfaf190bad655051dc8177444b4cff8706465
martinondiwa <surekondiwa@gmail.com>
2024-10-01 14:05:38 -0700

    asd

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,4 +16,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = sizei
+        self.__size = size
