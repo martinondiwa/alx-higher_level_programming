d4a60cca22f0364620ef7a13b2378599c012b157
martinondiwa <surekondiwa@gmail.com>
2024-10-15 12:44:54 -0700

    asd

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 
-def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
-        return f.write(text)
+import json
+
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
