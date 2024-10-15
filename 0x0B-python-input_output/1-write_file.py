ce562994d7233a65b3418aedb49ff9de2c7e2f1a
martinondiwa <surekondiwa@gmail.com>
2024-10-15 12:51:50 -0700

    asd

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+def write_file(filename="", text=""):
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
