0110b856de5ada4879c882c96e7424bbd2f1e0f5
martinondiwa <surekondiwa@gmail.com>
2024-10-15 12:50:32 -0700

    as

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 
-def read_lines(filename="", nb_lines=0):
-    with open(filename, "r", encoding="UTF-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        for index in range(nb_lines):
-            print(f.readline(), end="")
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
