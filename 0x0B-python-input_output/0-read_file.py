8c9443c63f36d55d58b191e22662fd4483732a55
martinondiwa <surekondiwa@gmail.com>
2024-10-15 13:05:27 -0700

    asd

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''Module for read_file method.'''
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    '''Method for reading from file.'''
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read()
+        print(text, end="")
