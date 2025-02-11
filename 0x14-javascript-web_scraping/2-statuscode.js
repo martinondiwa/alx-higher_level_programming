82e093fa8de174ae9c370f2d33de48d25aabd2c2
martinondiwa <surekondiwa@gmail.com>
2025-02-11 08:45:50 -0800

    asdf

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// A script that display the status code of a GET request
-
-const args = process.argv;
-let request = require('request');
-request(args[2], function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else console.log('code:', response && response.statusCode);
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
